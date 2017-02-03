cf47c2ac8cd48ca7fc58fd43a62b7e4e9abdbf4c
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-02-02 19:58:54 -0700

    Add app.listen

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('*', function(req, res) {
   res.send('Hello world.');
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
