9675c4db71e1751df4f162778060a79fbf5bed1d
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-02-02 17:47:39 -0700

    Fix typo

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
-var logger= require('morgan');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(logger('dev'));
 
-app.use(express.static(__dirname = '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.send('Hello world.');
