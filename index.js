ad914c1f44ff24f7a686af5148f540e7c88a8984
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-02-02 21:24:01 -0700

    Add all call page dependencies: proof-of-concept pug campaign template

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
-
 app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'pug')
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/call/:campaign(*)??', function(req, res) {
+  res.render('campaign_full', { title: 'A campaign page', message: 'This is the page for the ' + req.params.campaign + ' campaign.' })
+});
 
 app.get('*', function(req, res) {
   res.send('Hello world.');
